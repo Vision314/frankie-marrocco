646151a9fd46c67d958c6ec66b8dbfa6f7a082a9
Vision314 <42813883+Vision314@users.noreply.github.com>
2026-06-04 00:43:01 +0000

    Deploying to gh-pages from @ Vision314/frankie@7c7b2a846c126947004bc2155d71b2b81219167c 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://frankie.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://frankie-marrocco.github.io/frankie/#contact">contact us</a>.'
               }
             ]
           }
