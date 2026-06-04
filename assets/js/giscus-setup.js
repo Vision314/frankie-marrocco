22c5277a7ff84d8b55e1de919138331da3b4e3e0
Vision314 <42813883+Vision314@users.noreply.github.com>
2026-06-04 14:30:01 +0000

    Deploying to gh-pages from @ Vision314/frankie-marrocco@d6e090187b553ecb6e02080448d55cfccc3f82a9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Vision314/frankie",
+    "data-repo": "Vision314/frankie-marrocco",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
